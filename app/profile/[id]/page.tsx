0f086d4a563cf3e2a7116ca4d25971b0ceabf0d9
nabeel-weekend <nabeel.weekend@gmail.com>
2025-11-22 17:27:33 +0500

    FOLLOWING UNFOLLOWING WORKS

--- app/profile/[id]/page.tsx
+++ app/profile/[id]/page.tsx
@@ -1,10 +1,7 @@
+import UserDetails from "@/components/ButtonFollow";
 import { clerkClient } from "@clerk/nextjs/server";
 import { ArrowLeft } from "lucide-react";
 import Link from "next/link";
-import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar"
-import MatrixRain from "@/components/MatrixRain";
-import { formatUTCFromMS } from "@/lib/timeAgo";
-import ButtonFollow from "@/components/ButtonFollow";
 
 export default async function ProfilePage({ params }: { params: { id: string } }) {
   const { id } = await params;
@@ -25,34 +22,7 @@ export default async function ProfilePage({ params }: { params: { id: string } }
 
 
 
-      <section className="w-full border-b border-foreground/20">
-        <div className="w-full flex flex-col justify-between items-center gap-2">
-          <div className="w-full h-30">
-            <MatrixRain />
-          </div>
-
-          <div className="flex flex-col w-full px-5 pb-5">
-            <div className="flex w-full items-center justify-between">
-              <Avatar className="size-30 -mt-6">
-                <AvatarImage src={user.imageUrl} />
-                <AvatarFallback>K</AvatarFallback>
-              </Avatar>
-
-              <ButtonFollow userId={user.id} />
-            </div>
-
-            <div className="flex justify-between items-baseline w-full">
-              <div className="flex flex-col mt-5">
-                <h1 className="text-2xl font-bold">{user.fullName}</h1>
-                <h2 className="opacity-80">@{user.username}</h2>
-              </div>
-              <div className="flex flex-col">
-                <p className="text-sm opacity-80">Joined {formatUTCFromMS(user.createdAt)}</p>
-              </div>
-            </div>
-          </div>
-        </div>
-      </section>
+      <UserDetails userId={user.id} userImage={user.imageUrl} userName={user.username} userFullname={user.fullName} userCreatedAt={user.createdAt} />
     </>
   )
 }
--- components/ButtonFollow.jsx
+++ components/ButtonFollow.jsx
@@ -4,8 +4,11 @@ import { api } from "@/convex/_generated/api";
 import { useUser } from "@clerk/nextjs";
 import { useMutation, useQuery } from "convex/react";
 import { Spinner } from "./ui/spinner";
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar"
+import MatrixRain from "@/components/MatrixRain";
+import { formatUTCFromMS } from "@/lib/timeAgo";
 
-export default function ButtonFollow({ userId }) {
+export default function UserDetails({ userId, userImage, userName, userFullname, userCreatedAt }) {
   const addFollower = useMutation(api.user.addFollower);
   const removeFollower = useMutation(api.user.removeFollower);
 
@@ -16,26 +19,56 @@ export default function ButtonFollow({ userId }) {
 
   let isFollowing = false;
   if (Array.isArray(followers)) {
-    isFollowing = followers?.some((follower) => follower.userId === currentUser.id);
+    isFollowing = followers?.some((follower) => follower.followerId === currentUser.id);
   }
 
   let currentFollower = null;
   if (Array.isArray(followers)) {
-    currentFollower = followers?.find((follower) => follower.userId === currentUser.id);
+    currentFollower = followers?.find((follower) => follower.followerId === currentUser.id);
   }
 
+  const canAppear = userId !== currentUser.id;
+
   return (
-    <>
-      {!isFollowing ? (
-        <button onClick={() => {
-          addFollower({ followerId: currentUser.id, userId: userId })
-        }} className="py-2 hover:opacity-90 cursor-pointer transition-all duration-250 active:scale-95 px-6 bg-foreground text-background rounded-full">Follow</button>
-      )
-        : (
-          <button onClick={() => {
-            removeFollower({ followerId: currentFollower?._id })
-          }} className="py-2 hover:opacity-90 cursor-pointer transition-all duration-250 active:scale-95 px-6 bg-foreground text-background rounded-full">Unfollow</button>
-        )}
-    </>
+    <section className="w-full border-b border-foreground/20">
+      <div className="w-full flex flex-col justify-between items-center gap-2">
+        <div className="w-full h-30">
+          <MatrixRain />
+        </div>
+
+        <div className="flex flex-col w-full px-5 pb-5">
+          <div className="flex w-full items-center justify-between">
+            <Avatar className="size-30 -mt-6">
+              <AvatarImage src={userImage} />
+              <AvatarFallback>K</AvatarFallback>
+            </Avatar>
+
+            {canAppear && (
+              !isFollowing ? (
+                <button onClick={() => {
+                  addFollower({ followerId: currentUser.id, userId: userId })
+                }} className="py-2 hover:opacity-90 cursor-pointer transition-all duration-250 active:scale-95 px-6 bg-foreground text-background rounded-full">Follow</button>
+              )
+                : (
+                  <button onClick={() => {
+                    removeFollower({ followerId: currentFollower?._id })
+                  }} className="py-2 hover:opacity-90 cursor-pointer transition-all duration-250 active:scale-95 px-6 bg-foreground text-background rounded-full">Unfollow</button>
+                )
+            )}
+          </div>
+
+          <div className="flex justify-between items-baseline w-full">
+            <div className="flex flex-col mt-5">
+              <h1 className="text-2xl font-bold">{userFullname}</h1>
+              <h2 className="opacity-80">@{userName}</h2>
+            </div>
+            <div className="flex flex-col text-right">
+              <p className="text-sm opacity-80">Joined {formatUTCFromMS(userCreatedAt)}</p>
+              <p>{followers.length == 1 ? `${followers.length} Follower` : `${followers.length} Followers`}</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
   )
 }
