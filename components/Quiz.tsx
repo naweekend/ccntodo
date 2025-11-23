5b14c99efa1a9cfb892b1020117206569317d0e8
nabeel-weekend <nabeel.weekend@gmail.com>
2025-11-23 10:31:29 +0500

    changed iq home layout on mobile

--- components/Quiz.tsx
+++ components/Quiz.tsx
@@ -7,10 +7,10 @@ export default function Quiz() {
   return (
     <>
       <h1 className="sm:text-5xl text-3xl font-bold">The Real IQ Test</h1>
-      <div className="flex justify-center items-center gap-5 mt-5">
-        <TalkingEinstein stopAt={8200} className="lg:w-60 md:w-50 sm:w-30 w-20" />
+      <div className="flex sm:flex-row flex-col justify-center items-center gap-5 mt-5">
+        <TalkingEinstein stopAt={8200} className="lg:w-60 md:w-50 sm:w-30 w-1/2" />
         <SpeechBubble
-          className="h-50 max-w-md w-full sm:text-lg text-sm"
+          className="h-50 max-w-md w-full sm:text-lg text-md"
           text={`Well. Well. Well… another fledgling attempting to measure his wits.${"|"}${"\n"}This IQ test isn't exactly kindergarten, kiddo.${"|"} Brace yourself;${"|"} genius isn't handed out like candy.`}
           speed={40}
         />
